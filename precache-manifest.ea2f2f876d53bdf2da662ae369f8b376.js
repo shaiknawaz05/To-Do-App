6934f53213cfda305b8671e19a18b57725e7b712
shaiknawaz05 <shaiknawaz05@gmail.com>
2020-12-16 12:59:59 +0530

    Updates

---
+++ precache-manifest.ea2f2f876d53bdf2da662ae369f8b376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed68746b22d606046c0",
+    "url": "/To-Do-App/static/css/main.aedc2f62.chunk.css"
+  },
+  {
+    "revision": "1ed68746b22d606046c0",
+    "url": "/To-Do-App/static/js/main.1ed68746.chunk.js"
+  },
+  {
+    "revision": "2ebffe176c6a746a0823",
+    "url": "/To-Do-App/static/js/1.2ebffe17.chunk.js"
+  },
+  {
+    "revision": "fa3a1b47ecfa96bfa9ad",
+    "url": "/To-Do-App/static/js/runtime~main.fa3a1b47.js"
+  },
+  {
+    "revision": "aa613699b5c9252418a67918802de623",
+    "url": "/To-Do-App/index.html"
+  }
+];
